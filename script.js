3e06c96397a64a3280bc5d8c36950523ae40190a
Caleb Barton <99145883+barton-ce@users.noreply.github.com>
2023-11-09 09:58:44 -0600

    Finishes basic game

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 console.log("Hello World");
 
+
+let playerWin = 0;
+let computerWin = 0;
+
 function getComputerChoice() {
   let choice = ["rock", "paper", "scissors"];
   return choice[Math.floor(Math.random() * choice.length)]
 }
 
 // Need to figure out how to loop to play the game 5 times and keep a score then return the winner. 
-
 function game() {
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Enter Your Choice');
-    computerSelection = getComputerChoice();
-    let playerWin = 0;
-    let computerWin = 0;
 
-    if (playerSelection === computerSelection) {
-      return `The player chose ${playerSelection} and the computer chose ${computerSelection} so its a Tie! `;
-    }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-      playerWin++;
-      return `The player chose ${playerSelection} and the computer chose ${computerSelection} ${playerSelection} beats ${computerSelection} Player wins! The score is player: ${playerWin} to computer: ${computerWin} `;
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-      playerWin++;
-      return `The player chose ${playerSelection} and the computer chose ${computerSelection} ${playerSelection} beats ${computerSelection}. Player wins! The score is player: ${playerWin} to computer: ${computerWin}`;
-    }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-      playerWin++;
-      return `The player chose ${playerSelection} and the computer chose ${computerSelection} ${playerSelection} beats ${computerSelection} Player wins! The score is player: ${playerWin} to computer: ${computerWin}`;
-    }
-    else {
-      computerWin++;
-      return `The player chose ${playerSelection} and the computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}, Computer Wins. The score is computer: ${computerWin} to player: ${playerWin}`;
-    }
-  };
-  console.log(playRound());
+  while (playerWin < 5 && computerWin < 5) {
+    const playerSelection = prompt('Rock, Paper or Scissors').toLowerCase();
+    const computerSelection = getComputerChoice();
+    alert(playRound(playerSelection, computerSelection));
+  }
+  if (playerWin == 5) {
+    console.log("You Win! You are Amazing")
+  } else {
+    console.log("You lose! Beter luck next time.")
+  }
 }
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `The player chose ${playerSelection} and the computer chose ${computerSelection} so its a Tie! `;
+  }
+  else if (playerSelection === "rock" && computerSelection == "scissors") {
+    playerWin += 1;
+    return `The player chose ${playerSelection} and the computer chose ${computerSelection} ${playerSelection} beats ${computerSelection} Player wins! The score is player: ${playerWin} to computer: ${computerWin} `
+
+  }
+  else if (playerSelection === "scissors" && computerSelection == "paper") {
+    playerWin += 1;
+    return `The player chose ${playerSelection} and the computer chose ${computerSelection} ${playerSelection} beats ${computerSelection}. Player wins! The score is player: ${playerWin} to computer: ${computerWin}`;
+  }
+  else if (playerSelection === "paper" && computerSelection == "rock") {
+    playerWin += 1;
+    return `The player chose ${playerSelection} and the computer chose ${computerSelection} ${playerSelection} beats ${computerSelection} Player wins! The score is player: ${playerWin} to computer: ${computerWin}`;
+  }
+  else {
+    computerWin += 1;
+    return `The player chose ${playerSelection} and the computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}, Computer Wins. The score is computer: ${computerWin} to player: ${playerWin}`;
+
+  }
+};
+
+
+
+game();
